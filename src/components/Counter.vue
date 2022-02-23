4f4fbc2098147adddaf3cd73f1def2654563e417
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-02-23 20:51:13 +0800

    Implement a counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="count++" :style="exampleStyle">Count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      exampleStyle: {
+        color: 'red',
+        fontSize: '2em'
+      }
+    }
+  }
+}
+</script>
